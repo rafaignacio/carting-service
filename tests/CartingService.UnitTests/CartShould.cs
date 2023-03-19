e86178de72b9701c6d3ee546d916fe6d3aaed6e4
Rafael Ignacio Pinto <ignacio.rafael@gmail.com>
2023-03-19 12:50:09 +0100

    refactoring

--- tests/CartingService.UnitTests/CartShould.cs
+++ tests/CartingService.UnitTests/CartShould.cs
@@ -8,7 +8,7 @@ namespace CartingService.UnitTests;
 
 public class CartShould {
     [Fact]
-    public async Task Successfully_add_item() {
+    public async Task Successfully_add_item_and_retrieve_it() {
         var repository = new FakeCartRepository();
         var sut = new Cart( repository, new CartItemValidator() );
         var id = new CartId();
@@ -31,18 +31,69 @@ public class CartShould {
         var sut = new Cart( repository, new CartItemValidator() );
         var id = new CartId();
 
-        var addItemResult = await sut.AddItem(id, new CartItem(1, "Mouse", null, 1.0M, 1));
-        addItemResult.Match<bool>(
-            _ => true,
-            _ => false,
-            _ => false ).Should().BeTrue();
-
+        await sut.AddItem(id, new CartItem(1, "Mouse", null, 1.0M, 1));
         await sut.DeleteItem(id, 1);
 
         var cartResult = await sut.GetById(id);
         cartResult.Match(
             list => list.Count,
             _ => 0 ).Should().Be(0);
+    }
+
+    [Fact]
+    public async Task Indicate_not_found_when_retrieving_inexistent_cart() {
+        var repository = new FakeCartRepository();
+        var sut = new Cart( repository, new CartItemValidator() );
+
+        var cartResult = await sut.GetById(new CartId());
+        cartResult.Match(
+            _ => false,
+            _ => true ).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Indicate_not_found_when_removing_inexistent_item() {
+        var repository = new FakeCartRepository();
+        var sut = new Cart( repository, new CartItemValidator() );
+        var id = new CartId();
+
+        await sut.DeleteItem(id, 1);
+
+        var cartResult = await sut.GetById(id);
+        cartResult.Match(
+            _ => false,
+            _ => true ).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Indicate_error_when_adding_item_throws_exception() {
+        var repository = new FakeCartRepository()
+            .ThrowExceptionWhenCallingAddItem();
+        var sut = new Cart( repository, new CartItemValidator() );
+        var id = new CartId();
+
+        var addItemResult = await sut.AddItem(id, new CartItem(1, "Mouse", null, 1.0M, 1));
+        addItemResult.Match<bool>(
+            _ => false,
+            _ => false,
+            _ => true ).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Indicate_error_when_removing_item_throws_exception() {
+        var repository = new FakeCartRepository()
+            .ThrowExceptionWhenCallingRemoveItem();
+        var sut = new Cart( repository, new CartItemValidator() );
+        var id = new CartId();
+
+        await sut.AddItem(id, new CartItem(1, "Mouse", null, 1.0M, 1));
+        var deleteResult = await sut.DeleteItem(id, 1);
+
+        deleteResult.Match(
+            _ => false,
+            _ => false,
+            _ => true
+        ).Should().BeTrue();
     }
 
     [Fact]
--- tests/CartingService.UnitTests/Fakes/FakeCartRepository.cs
+++ tests/CartingService.UnitTests/Fakes/FakeCartRepository.cs
@@ -6,14 +6,33 @@ namespace CartingService.UnitTests.Fakes;
 public record FakeCartRepository : ICartRepository
 {
     private Dictionary<CartId, List<CartItem>> _data = new();
+    private bool _throw_exception_add_item = false;
+    private bool _throw_exception_remove_item = false;
+
+    public FakeCartRepository ThrowExceptionWhenCallingAddItem() => 
+        this with {
+            _throw_exception_add_item = true
+        };
+
+    public FakeCartRepository ThrowExceptionWhenCallingRemoveItem() => 
+        this with {
+            _throw_exception_remove_item = true
+        };
 
     private bool IsCartRegistered(CartId id) => 
         _data.ContainsKey( id );
 
+    private void RegisterCartIfNotPresent(CartId id) {
+        if( !IsCartRegistered( id ) )
+            _data.Add(id, new());
+    }
+
     public Task AddItem(CartId cartId, CartItem item)
     {
-        if( !IsCartRegistered( cartId ) )
-            _data.Add(cartId, new());
+        if(_throw_exception_add_item)
+            throw new Exception("Failed to add item.");
+
+        RegisterCartIfNotPresent(cartId);
 
         var cart = _data[cartId];
         cart.Add(item);
@@ -23,6 +42,10 @@ public record FakeCartRepository : ICartRepository
 
     public Task Delete(CartId cartId, int itemId)
     {
+        if(_throw_exception_remove_item)
+            throw new Exception("Failed to remove item.");
+
+        RegisterCartIfNotPresent(cartId);
         _data[cartId].RemoveAll( item => item.Id == itemId );
 
         return Task.CompletedTask;
@@ -30,16 +53,14 @@ public record FakeCartRepository : ICartRepository
 
     public Task<List<CartItem>> GetById(CartId id)
     {
-        if( !IsCartRegistered( id ) )
-            _data.Add(id, new());
+        RegisterCartIfNotPresent(id);
 
         return Task.FromResult(_data[id]);
     }
 
     public Task<CartItem?> GetCartItemById(CartId cartId, int id)
     {
-        if( !IsCartRegistered( cartId ) )
-            _data.Add(cartId, new());
+        RegisterCartIfNotPresent(cartId);
 
         return Task.FromResult( _data[cartId].SingleOrDefault( i => i.Id == id ) );
     }
